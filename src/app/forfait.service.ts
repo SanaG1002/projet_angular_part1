6fca9515f08271384027c28d3776126613647169
SanaG1002 <92936779+SanaG1002@users.noreply.github.com>
2023-02-24 23:00:12 -0500

    Correction  ajout modification supprimer

--- src/app/forfait.service.ts
+++ src/app/forfait.service.ts
@@ -25,15 +25,19 @@ export class ForfaitService {
 
   
   addForfait(forfait : Forfait): Observable<void> {
-    forfait.dateDebut = formatDate(forfait.dateDebut, 'DD-MM-YYYY', 'en-US');
-    forfait.dateFin = formatDate(forfait.dateFin, 'DD-MM-YYYY', 'en-US');
+    console.log(forfait.dateDebut);
+    console.log(formatDate(forfait.dateDebut, 'YYYY-MM-dd', 'en-US'));
+    console.log(forfait.dateFin);
+    console.log(formatDate(forfait.dateFin, 'YYYY-MM-dd', 'en-US'));
+    forfait.dateDebut = formatDate(forfait.dateDebut, 'YYYY-MM-dd', 'en-US');
+    forfait.dateFin = formatDate(forfait.dateFin, 'YYYY-MM-dd', 'en-US');
     console.log(forfait);
     return this.http.post<void>(this.API_URL, forfait, httpOptions);
   }
 
   updateForfait(forfait: Forfait): Observable<void> {
-    forfait.dateDebut = formatDate(forfait.dateDebut, 'DD-MM-YYYY', 'en-FR');
-    forfait.dateFin = formatDate(forfait.dateFin, 'DD-MM-YYYY', 'en-FR');
+    forfait.dateDebut = formatDate(forfait.dateDebut, 'YYYY-MM-dd', 'en-US');
+    forfait.dateFin = formatDate(forfait.dateFin, 'YYYY-MM-dd', 'en-US');
     return this.http.put<void>(`${this.API_URL}?id=${forfait.id}`, forfait, httpOptions);
   }
 
